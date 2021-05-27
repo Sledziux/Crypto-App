a3fb468edf87b8696fbe29dc88fd011e58d6169b
Sledziux <sledziuxjp@gmail.com>
2021-05-27 18:40:43 +0200

    Market Screen Added

--- components/index.js
+++ components/index.js
@@ -1,6 +1,8 @@
 import BalanceInfo from "./BalanceInfo";
 import Chart from "./Chart";
+import HeaderBar from "./HeaderBar";
 import IconTextButton from "./IconTextButton";
 import TabIcon from "./TabIcon";
-BalanceInfo;
-export { TabIcon, IconTextButton, BalanceInfo, Chart };
+import TextButton from "./TextButton";
+
+export { TabIcon, IconTextButton, BalanceInfo, Chart, HeaderBar, TextButton };
